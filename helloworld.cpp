bbf077948190f949734098d26c1b81cfbb76e655
codeasm <nico.vijlbrief@outlook.com>
2019-09-02 21:39:19 +0200

    OpenGL Helloworld compiling works

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,18 +1,31 @@
- #include <iostream>
- #include <vector>
- #include <string>
+#include "GL/freeglut.h"
+#include "GL/gl.h"
 
- using namespace std;
+void drawTriangle()
+{
+    glClearColor(0.4, 0.4, 0.4, 0.4);
+    glClear(GL_COLOR_BUFFER_BIT);
 
- int main()
- {
+    glColor3f(1.0, 1.0, 1.0);
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 
-     vector<string> msg {"Hello", "C++", "World", "from", "VS Code!", "and the C++ extension!"};
+        glBegin(GL_TRIANGLES);
+                glVertex3f(-0.7, 0.7, 0);
+                glVertex3f(0.7, 0.7, 0);
+                glVertex3f(0, -1, 0);
+        glEnd();
 
-    
-     for (const string& word : msg)
-     {
-         cout << word << " ";
-     }
-     cout << endl;
- }
+    glFlush();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("OpenGL - Creating a triangle");
+    glutDisplayFunc(drawTriangle);
+    glutMainLoop();
+    return 0;
+}
