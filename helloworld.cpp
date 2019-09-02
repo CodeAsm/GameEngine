464b2bc8d3330cd1aae3cbb51843ea1d0078c4de
codeasm <nico.vijlbrief@outlook.com>
2019-09-02 22:26:20 +0200

    Glew and GLFW for tutorial fixed

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,20 @@
 
 #include "iostream"
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
 
 int main(int argc, char **argv)
 {
 
     printf("[APP] Starting ...\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     printf("[APP] Stopping ...\n");
     return 0;
 }
